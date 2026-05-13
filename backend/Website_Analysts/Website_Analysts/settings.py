4e600e88c4fa64d6a020cb2c249def45c01a0f5d
ImongMama45 <quertrussellalisan@gmail.com>
2026-05-14 04:01:12 +0800

    Update settings.py

--- backend/Website_Analysts/Website_Analysts/settings.py
+++ backend/Website_Analysts/Website_Analysts/settings.py
@@ -234,6 +234,7 @@ CORS_ALLOWED_ORIGINS = [
 
 CORS_ALLOW_ALL_ORIGINS = False
 CORS_ALLOW_ALL_ORIGINS = DEBUG  # Only in development
+CORS_ALLOW_CREDENTIALS = True  # ONLY if using cookies/session
 
 CORS_ALLOW_HEADERS = [
     'accept',
